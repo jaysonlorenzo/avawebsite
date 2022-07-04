fed5e672cc51a444ee0619b10e7c67f74721c1fb
jaysonlorenzo <jayson.lorenzo2577@gmail.com>
2022-07-04 09:31:22 +0800

    Patch

--- src/components/ROI.js
+++ src/components/ROI.js
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 export default function CardTable({ color }) {
   return (
     <>
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
-            <div className="flex flex-wrap mt-4">
+        <div className=" md:px-10 mx-auto w-full">
+            <div className="flex flex-wrap">
                 <div className="w-full mb-12 px-4">
                     <div
                         className={
---
+++ src/pages/Contactform.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Seo from "../components/seo"
+import Layout from "../components/layout"
+import CF from "../components/Contactform"
+
+
+const ctct = () => (
+  
+    <Layout>
+        <Seo title="Contact form"/>
+        <CF />
+    </Layout>
+
+)
+
+export default ctct
